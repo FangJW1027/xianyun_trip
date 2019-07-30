e8911bcbe622cf25f3082eb339da0e179d763351
fangjianwei <94414456@qq.com>
2019-07-30 19:28:00 +0800

    header&footer

---
+++ layouts/default.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- nuxt组件占位符 -->
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header'
+import Footer from '@/components/footer'
+
+export default {
+  
+  // 注册组件
+  components:{
+    Header,
+    Footer
+  }
+}
+</script>
+
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+ul,li,ol{
+  list-style:none;
+}
+
+a{
+  color:inherit;
+  text-decoration:none;
+}
+
+em,i{
+  font-size:normal;
+}
+</style>
